5cfc42d473d63cce2fd6a2fe40f5f3b5c540b6a2
Ryan <42ryan.curry@gmail.com>
2021-09-03 14:46:43 +1200

    Setup cartographer for lidar use

--- cartographer_ros/configuration_files/my_robot.lua
+++ cartographer_ros/configuration_files/my_robot.lua
@@ -28,10 +28,10 @@ options = {
   use_odometry = true,
   use_nav_sat = false,
   use_landmarks = false,
-  num_laser_scans = 0,
+  num_laser_scans = 1,
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 1,
-  num_point_clouds = 1,
+  num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 5e-3,
@@ -46,7 +46,7 @@ options = {
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
-TRAJECTORY_BUILDER_2D.min_range = 0.2
+TRAJECTORY_BUILDER_2D.min_range = 0.3
 TRAJECTORY_BUILDER_2D.max_range = 8
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 4
 TRAJECTORY_BUILDER_2D.min_z = 0
@@ -58,15 +58,15 @@ TRAJECTORY_BUILDER_2D.max_z = 0.3
 
 
 
-POSE_GRAPH.optimize_every_n_nodes = 5
+POSE_GRAPH.optimize_every_n_nodes = 50
 TRAJECTORY_BUILDER_2D.use_imu_data = true
 
 
---POSE_GRAPH.optimization_problem.local_slam_pose_translation_weight = 1e12
---POSE_GRAPH.optimization_problem.local_slam_pose_rotation_weight = 1e12
---POSE_GRAPH.optimization_problem.acceleration_weight = 1e-12
---POSE_GRAPH.optimization_problem.rotation_weight = 1e6
---POSE_GRAPH.optimization_problem.odometry_translation_weight = 1e6
---POSE_GRAPH.optimization_problem.odometry_rotation_weight = 1e1
+POSE_GRAPH.optimization_problem.local_slam_pose_translation_weight = 1e24
+POSE_GRAPH.optimization_problem.local_slam_pose_rotation_weight = 1e24
+POSE_GRAPH.optimization_problem.acceleration_weight = 1e-12
+POSE_GRAPH.optimization_problem.rotation_weight = 1e12
+POSE_GRAPH.optimization_problem.odometry_translation_weight = 1e6
+POSE_GRAPH.optimization_problem.odometry_rotation_weight = 1e1
 
 return options
